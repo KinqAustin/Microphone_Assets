5c422383a2a9033287060e0ea2d7e15e9fe79c7d
KinqAustin <33954136+KinqAustin@users.noreply.github.com>
2017-11-26 18:50:43 -0600

    Add files via upload

---
+++ .gitignore/MP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MP : MonoBehaviour {
+
+	public AudioSource AS;
+
+	void Start () {
+		AS = GetComponent<AudioSource> ();
+		AS.clip = Microphone.Start ("Logitech USB Headset",false,10,44100);
+		AS.Play ();
+		foreach (string device in Microphone.devices) {
+
+			Debug.Log (device);
+
+		}
+
+	}
+	
+
+}
